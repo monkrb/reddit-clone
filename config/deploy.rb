99dc93e161435469e502b4797cb85e9e1f4afe7a
Damian Janowski <damian.janowski@gmail.com>
2009-07-30 16:29:28 -0300

    Preparing for deploy.

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+# after "deploy:update_code", "deploy:set_permissions"
+after "deploy:update_code", "deploy:symlink_configs"
+
+namespace :deploy do
+  desc "Start the app"
+  task :start, :roles => :app do
+    sudo "a2ensite #{vhost}"
+    sudo "/etc/init.d/apache2 reload"
+  end
+
+  desc "Stop the app"
+  task :stop, :roles => :app do
+    sudo "a2dissite #{vhost}"
+    sudo "/etc/init.d/apache2 reload"
+  end
+
+  desc "Restart the app"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :set_permissions, :roles => :app do
+    run "chmod -R o+w #{release_path}/public"
+  end
+
+  desc "Symlink config files"
+  task :symlink_configs, :roles => :app, :except => { :no_symlink => true } do
+    %w(settings).each do |filename|
+      run "ln -nfs #{shared_path}/config/#{filename}.yml #{release_path}/config/#{filename}.yml"
+    end
+  end
+end
